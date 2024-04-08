e8f28206f6066ed4aa91f4009dc81088bbf20e2c
Mel Koppens <mkoppens12@hotmail.com>
2024-04-08 01:15:12 -0700

    second save

---
+++ React-practice/src/box.jsx
@@ -0,0 +1,30 @@
+// import React from 'react';
+// import './App.css';
+
+// const Box = () => {
+//   // console.log(props);
+//   return (
+//     <div className="box">
+//       I'm a box
+//       {/* <p>Cohort {props.cohortNumber} Is Awesome!</p>
+//       <button>Click Me</button> */}
+//     </div>
+//   );
+// };
+
+// export default Box;
+
+import React from 'react';
+
+const Box = () => {
+  return (
+    <div>
+      I'm a box!
+      <p>Cohort Is Awesome!</p>
+      <button>Click Me</button>
+    </div>
+  );
+};
+// {props.cohortNumber}
+
+export default Box;
